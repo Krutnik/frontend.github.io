4e7f4c3fe6d0c00248b9ac01c7b19b57d68de4af
Krutnik <67455038+Krutnik@users.noreply.github.com>
2020-11-23 17:32:43 +0300

    31 задачка
    решение задачки №31

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = +prompt("Введите целое число");
+
+function checkNumberDivide(num) {
+    let flag = false;
+    
+  for(let i = 2; i < num; i++) {
+        if(num % i == 0) {
+            flag = true;
+        }
+   }
+    return flag;
+}
+alert(checkNumberDivide(number));
