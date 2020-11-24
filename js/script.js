420439f80e4a65df84e498cea36504554f131443
Krutnik <67455038+Krutnik@users.noreply.github.com>
2020-11-24 15:35:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 let btnCreate = document.querySelector('.btn_create');
 let btnRemove = document.querySelector('.btn_remove');
-let newElements = document.querySelector('new_elemnts');
+let newElements = document.querySelector('.new_elements');
 let elem;
 let numberElements = document.querySelector('.number_elements');
 
+
 btnCreate.onclick = function() {
-    for(let i = 0; i < +numberElements.value; i++) {}
+    for(let i = 0; i < +numberElements.value; i++) {
+
         elem = document.createElement('p');
         elem.textContent = "Новый абзац...";
         elem.classList.add('elem');
+
+        if(i == 0) {
+            elem.style.backgroundColor = "darkred";
+            elem.style.color = "white";
+        }
 
         newElements.append(elem);
     }
 }
 
+let children = newElements.childNodes;
+
 btnRemove.onclick = function() {
-    newElements.childNodes[0].remove;
+    for(let i = children.length - 1; i > -1; i--) {
+        newElements.removeChild(children[i]);
+    }
 }
