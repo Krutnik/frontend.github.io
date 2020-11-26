8b7b363d67cfb1ee4d960fd0d069b4c3f65e0da9
Krutnik <67455038+Krutnik@users.noreply.github.com>
2020-11-26 18:15:16 +0300

    26.11

---
+++ js/createTable.js
@@ -0,0 +1,58 @@
+let numberRows = document.getElementById('numberRows')/value;
+let numberColumns = document.getElementById('numberColumns').value;
+
+let tableBorderWidth = document.getElementById('tableBorderWidth').value + 'px';
+let tableBorderType = document.querySelector('.table-border-type').value;
+let tableBorderColor = document.getElementById('tableBorderColor').value;
+
+let tabledataPadding = document.getElementById('tableDataPadding').value;
+
+let tableDataColor = document.getElementById('tableDataColor').value;
+let tableFontSize = document.querySelector('.table-font-size').value;
+let tableFontColor = document.getElementById('tableDataFontColor').value;
+
+let tableDataWidth = document.getElementById('tableDataWidth').value;
+let dataBorderType = document.querySelector('.data-border-type').value;
+let dataBorderColor = document.getElementById('dataBorderColor').value;
+
+let btnCreate = document.querySelector('.btn-create');
+let btnRemove = document.querySelector('.btn-remove');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
