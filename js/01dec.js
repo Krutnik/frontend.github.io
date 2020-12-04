3d75902343288429398e70f79b4146104ac01d4d
Кирилл <67455038+Krutnik@users.noreply.github.com>
2020-12-04 13:42:53 +0300

    update

---
+++ js/01dec.js
@@ -0,0 +1,6 @@
+let btnDefault = document.querySelector('.btn-default');
+let intro = document.querySelector('.intro');
+
+btnDefault.addEventListener('click', () => { alert('клик по кнопке!'); });
+
+intro.style.border = "5px dashed gold";
